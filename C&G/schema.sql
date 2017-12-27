c70aa5f579710f85006dd10e3001300638bf6e78
Surya Vamsee <vsurya.ayyagari@gmail.com>
2017-12-27 10:17:54 +0530

    add grievances

---
+++ C&G/schema.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE IF NOT EXISTS `cng`;
+
+CREATE TABLE IF NOT EXISTS `grievance_records` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(40),
+  `id_no` varchar(40),
+  `email` varchar(40),
+  `phno` varchar(40),
+  `rmno` varchar(40),
+  `gr` varchar(40),
+  `gr_others` varchar(40),
+  `message` text,
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `grievance_records` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(40),
+  `body` varchar(40),
+  `message` text,
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
