90333942fa98c155b68acad11a39d07c7b433de8
Surya Vamsee <vsurya.ayyagari@gmail.com>
2017-12-28 15:31:56 +0530

    small UI changes

--- notices/Notice.php
+++ notices/Notice.php
@@ -10,14 +10,10 @@ $result=mysqli_query($con, $sql);
 <table width="90%" border="0" align="center" cellpadding="3" cellspacing="1" bgcolor="#CCCCCC">
 	<tr>
 		<td width="6%" align="center" bgcolor="#E6E6E6"><strong>#</strong></td>
-		<td width="53%" align="center" bgcolor="#E6E6E6"><strong>Notice</strong></td>
-		<td width="15%" align="center" bgcolor="#E6E6E6"><strong>Club/Department</strong></td>
+		<td width="15%" align="center" bgcolor="#E6E6E6"><strong>Notice</strong></td>
+		<td width="53%" align="center" bgcolor="#E6E6E6"><strong>Details</strong></td>
 		<td width="13%" align="center" bgcolor="#E6E6E6"><strong>Date/Time</strong></td>
 	</tr>
-	<tr>
-	<td colspan="5" align="right" bgcolor="#E6E6E6"><a href="new_topic.php"><strong>Create New Notice</strong> </a></td>
-	</tr>
-</table>
 <?php
  
 
@@ -34,4 +30,9 @@ while($rows = mysqli_fetch_array($result, MYSQLI_ASSOC)){
 }
 mysqli_close($con);
 ?>
+
+</table>
+<tr>
+	<td colspan="5" align="right" bgcolor="#E6E6E6"><a href="new_topic.php"><strong>Create New Notice</strong> </a></td>
+	</tr>
  <!-- create new topic -->
