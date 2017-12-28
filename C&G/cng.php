e54b6f3de6d3a87f21e7f7f010a5765253ac7417
Vaibhav Bhasin <bhasinvaibhav97@gmail.com>
2017-12-28 13:29:16 +0000

    front: init basic front end for cng and contact

--- C&G/cng.php
+++ C&G/cng.php
@@ -30,7 +30,7 @@ if(isset($_POST['name']) &&
 	   !empty($phno)&&
 	   !empty($rmno)&&
 	   !empty($gr)&&
-	   !empty($message)) 	
+	   !empty($message))
 	{
 		if($gr == 'Other' && empty($gr_others))
 		{
@@ -46,7 +46,7 @@ if(isset($_POST['name']) &&
 												        '".mysqli_real_escape_string($var,$gr)."',
 												        '".mysqli_real_escape_string($var,$gr_others)."',
 												        '".mysqli_real_escape_string($var,$message)."')";
-			
+
 			if($query_run = mysqli_query($var,$query))
 			{
 				$msg = "Your from has been submitted";
@@ -55,13 +55,13 @@ if(isset($_POST['name']) &&
 			{
 				die(mysqli_error($var));
 			}
-		}	
+		}
 	}
 	else{
 		$error2 = "Fill in all the fields";
 	}
 }
-	
+
 ?>
 
 <html>
@@ -81,22 +81,31 @@ if(isset($_POST['name']) &&
 					});
 			});
 		</script>
+		<link rel="stylesheet" href="../css/style.css" />
 	</head>
 	<body>
-		<form class="form" action="cng.php" method="POST" enctype="multipart/form-data">
-			NAME : <input type="text" name="name"><br>
-			ID No. : <input type="text" name="id"><br>
-			Bits Email ID : <input type="text" name="email"><br>
-			Phone Number : <input type="text" name="phno"><br>
-			Room Number : <input type="text" name="rmno"><br>
-			Grievance ON :<br>  
-				<input type="radio" id="gr_choice_1" name="gr" value="EC"><label for="gr_choice_1">EC</label><br>
-				<input type="radio" id="gr_choice_2" name="gr" value="CRC"><label for="gr_choice_2">CRC</label><br>
-				<input type="radio" id="gr_choice_3" name="gr" value="Other"><label for="gr_choice_3">Others (Please Specify)</label><br>
-				<input type="text" name="gr_others" id="gr_others"><br>
-			Grievance :<br> <textarea id="message" name="message" placeholder="Type Your Message Here" rows="6" cols="40"></textarea>
-			<input class="fomr" type="submit" name="submit" value="Submit">	
-			<p><?php echo $error1.$error2.$msg; ?></p>
-		</form>
+		<div id="wrapper" class="form-con">
+			<h2 id="cng-head">Contact and Grievances</h2>
+			<p>
+				Please fill the following form to reach out to us.
+			</p>
+			<div class="formmm"
+			<form class="form" action="cng.php" method="POST" enctype="multipart/form-data">
+				<label>Name :</label> <input type="text" name="name"><br>
+				<label>ID No. :</label> <input type="text" name="id"><br>
+				<label>BITS Email ID :</label> <input type="text" name="email"><br>
+				<label>Phone Number :</label> <input type="text" name="phno"><br>
+				<label>Room Number :</label> <input type="text" name="rmno"><br>
+				<label>Grievance ON :</label><br>
+					<input type="radio" id="gr_choice_1" name="gr" value="EC"><label for="gr_choice_1">EC</label><br>
+					<input type="radio" id="gr_choice_2" name="gr" value="CRC"><label for="gr_choice_2">CRC</label><br>
+					<input type="radio" id="gr_choice_3" name="gr" value="Other"><label for="gr_choice_3">Others (Please Specify)</label><br>
+					<input type="text" name="gr_others" id="gr_others"><br>
+				<label>Grievance :</label><br> <textarea id="message" name="message" placeholder="Type Your Message Here" rows="6" cols="40"></textarea>
+				<input class="fomr submit" type="submit" name="submit" value="Submit">
+				<p><?php echo $error1.$error2.$msg; ?></p>
+			</form>
+		</div>
+
 	</body>
 </html>
