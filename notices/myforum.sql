91c98a7d23dd6d05bc3533f81366b127c371f321
ayushiaks <f2016587@hyderabad.bits-pilani.ac.in>
2018-01-03 12:51:50 +0530

    update db

--- notices/myforum.sql
+++ notices/myforum.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Dec 25, 2017 at 01:03 PM
+-- Generation Time: Jan 03, 2018 at 12:48 PM
 -- Server version: 5.7.20-0ubuntu0.16.04.1
 -- PHP Version: 7.0.26-2+ubuntu16.04.1+deb.sury.org+2
 
@@ -23,6 +23,43 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(4) NOT NULL,
+  `name` varchar(65) NOT NULL,
+  `admin` int(4) NOT NULL,
+  `email` varchar(65) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `name`, `admin`, `email`) VALUES
+(1, 'ayushi', 0, 'f20160587@hyderabad.bits-pilani.ac.in'),
+(2, 'SU', 1, ''),
+(3, 'Clubs', 2, '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `club`
+--
+
+CREATE TABLE `club` (
+  `event_name` varchar(65) NOT NULL,
+  `detail` varchar(255) NOT NULL,
+  `poster` varchar(255) NOT NULL,
+  `location` varchar(65) NOT NULL,
+  `id` int(4) NOT NULL,
+  `datetime` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `fanswer`
 --
 
@@ -35,6 +72,12 @@ CREATE TABLE `fanswer` (
   `a_datetime` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `fanswer`
+--
+
+
+
 -- --------------------------------------------------------
 
 --
@@ -42,15 +85,16 @@ CREATE TABLE `fanswer` (
 --
 
 CREATE TABLE `fquestions` (
-  `id` int(4) NOT NULL,
+  `int` int(4) NOT NULL,
   `topic` varchar(255) NOT NULL,
-  `detail` text NOT NULL,
-  `email` varchar(65) NOT NULL,
-  `datetime` varchar(65) NOT NULL,
-  `view` int(4) NOT NULL,
-  `name` varchar(65) NOT NULL,
-  `comments` int(5) NOT NUll
+  `detail` longtext NOT NULL,
+  `datetime` varchar(65) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `fquestions`
+--
+
 
 -- --------------------------------------------------------
 
@@ -58,19 +102,60 @@ CREATE TABLE `fquestions` (
 -- Table structure for table `newtopic`
 --
 
+CREATE TABLE `newtopic` (
+  `username` varchar(65) NOT NULL,
+  `password` varchar(65) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `newtopic`
+--
+
+
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `club`
+--
+ALTER TABLE `club`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `fanswer`
+--
+ALTER TABLE `fanswer`
+  ADD PRIMARY KEY (`question_id`);
 
 --
 -- Indexes for table `fquestions`
 --
 ALTER TABLE `fquestions`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`int`);
 
 --
+-- Indexes for table `newtopic`
+--
+ALTER TABLE `newtopic`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
 --
 -- AUTO_INCREMENT for table `fquestions`
 --
 ALTER TABLE `fquestions`
-  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT;
+  MODIFY `int` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
