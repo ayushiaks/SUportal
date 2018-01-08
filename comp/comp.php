491583049cce9d3baf85a0638a09c72dd7143d95
Vaibhav Bhasin <bhasinvaibhav97@gmail.com>
2018-01-08 16:13:45 +0000

    front-end: add missing styles

--- comp/comp.php
+++ comp/comp.php
@@ -7,12 +7,12 @@ $msg = "";
 
 $var = mysqli_connect("localhost", "root", "");
 if(isset($_POST['name']) &&
-	isset($_POST['id']) &&
-	isset($_POST['email']) &&
-	isset($_POST['phno'])&&
-	isset($_POST['rmno'])&&
-	isset($_POST['compon'])&&
-	isset($_POST['message']))
+isset($_POST['id']) &&
+isset($_POST['email']) &&
+isset($_POST['phno'])&&
+isset($_POST['rmno'])&&
+isset($_POST['compon'])&&
+isset($_POST['message']))
 {
 	$name = $_POST['name'];
 	$id = $_POST['id'];
@@ -23,21 +23,21 @@ if(isset($_POST['name']) &&
 	$compon = $_POST['compon'];
 
 	if(!empty($name) &&
-	   !empty($id) &&
-	   !empty($email) &&
-	   !empty($phno)&&
-	   !empty($rmno)&&
-	   !empty($compon)&&
-	   !empty($message)) 	
+	!empty($id) &&
+	!empty($email) &&
+	!empty($phno)&&
+	!empty($rmno)&&
+	!empty($compon)&&
+	!empty($message))
 	{
 		$query = "INSERT INTO `cng`.`complaint_records` VALUES ('','".mysqli_real_escape_string($var,$name)."',
-												        '".mysqli_real_escape_string($var,$id)."',
-												        '".mysqli_real_escape_string($var,$email)."',
-												        '".mysqli_real_escape_string($var,$phno)."',
-												        '".mysqli_real_escape_string($var,$rmno)."',
-												        '".mysqli_real_escape_string($var,$compon)."',
-												        '".mysqli_real_escape_string($var,$message)."')";
-			
+		'".mysqli_real_escape_string($var,$id)."',
+		'".mysqli_real_escape_string($var,$email)."',
+		'".mysqli_real_escape_string($var,$phno)."',
+		'".mysqli_real_escape_string($var,$rmno)."',
+		'".mysqli_real_escape_string($var,$compon)."',
+		'".mysqli_real_escape_string($var,$message)."')";
+
 		if($query_run = mysqli_query($var,$query))
 		{
 			$msg = "Your from has been submitted";
@@ -46,59 +46,43 @@ if(isset($_POST['name']) &&
 		{
 			die(mysqli_error($var));
 		}
-			
+
 	}
 	else{
 		$error2 = "Fill in all the fields";
 	}
 }
-	
+
 ?>
 
 <html>
-	<head>
-		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-      <!--Import materialize.css-->
-    	<link type="text/css" rel="stylesheet" href="../materialize/css/materialize.min.css"  media="screen,projection"/>
-
-      <!--Let browser know website is optimized for mobile-->
-    	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-		<link rel="stylesheet" href="../css/complaints.css" />
-	</head>
-	<body class="#42a5f5">
-		<div class="row">
-			<form id="form" class="col s12 #e3f2fd blue lighten-5 z-depth-3" action="comp.php" method="POST" enctype="multipart/form-data">
+<head>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+</head>
+<body>
+	<div class="row">
+		<form class="form" action="comp.php" method="POST" enctype="multipart/form-data">
 			<h4>Register Complaint</h4>
-				<div class="div">
-					<div class="input-field col s12">
-						<input type="hidden" name="name" value="<?php echo $name;?>" required>
-					</div>
-					<div class="input-field col s12">	
-						<input placeholder="ID" type="text" name="id" required>
-					</div>
-					<div class="input-field col s12">	
-						<input type="hidden" name="email" value="<?php echo $email;?>" required>
-					</div>
-					<div class="input-field col s12">	
-						<input placeholder="Phone Number" type="text" name="phno" required>
-					</div>
-					<div class="input-field col s12">	
-						<input placeholder="Room Number" type="text" name="rmno" required>
-					</div>
-					<div class="input-field col s12">
-						<input placeholder="Complaint On" type="text" name="compon" required>
-					</div>
-					<div class="input-field col s12">	
-						<textarea class="materialize-textarea" name="message" placeholder="Type Your Complaint Here"></textarea>
-					</div>	
-						<div ><input class="z-depth-4 button" type="submit" name="submit" value="Submit"><a  style="margin-left:35%; font-size:20px;" class="z-depth-4 button co_anonymous">Fill Anonymously</a></div>	
-						<p style="margin:1%; text-align:center; color:red; font-size: 20px;"><?php echo $error1.$error2.$msg; ?></p>
-				</div>		
-			</form>
-		</div>	
+			<input type="hidden" name="name" value="<?php echo $name;?>" required>
+			<input placeholder="ID" type="text" name="id" required>
+			<input type="hidden" name="email" value="<?php echo $email;?>" required>
+			<input placeholder="Phone Number" type="text" name="phno" required>
+			<input placeholder="Room Number" type="text" name="rmno" required>
 
-  <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-  <script type="text/javascript" src="js/materialize.min.js"></script>
-	</body>
+			<input placeholder="Complaint On" type="text" name="compon" required>
+
+			<p><textarea name="message" placeholder="Type Your Complaint Here"></textarea></p>
+
+			<input class="submit" type="submit" name="submit" value="Submit">
+			<input class="submit co_anonymous" type="submit" value="Fill Anonymously">
+			<p><?php echo $error1.$error2.$msg; ?></p>
+
+		</form>
+	</div>
+
+
+</body>
+
+
+
 </html>
--- comp/grievances.php
+++ comp/grievances.php
@@ -6,15 +6,15 @@ $msg = "";
 
 $var = mysqli_connect("localhost", "root", "");
 if(isset($_POST['name']) &&
-	isset($_POST['id']) &&
-	isset($_POST['email']) &&
-	isset($_POST['phno'])&&
-	isset($_POST['rmno'])&&
-	isset($_POST['gr'])&&
-	isset($_POST['gr_others'])&&
-	isset($_POST['gr_to'])&&
-	isset($_POST['gr_to_others'])&&
-	isset($_POST['message']))
+isset($_POST['id']) &&
+isset($_POST['email']) &&
+isset($_POST['phno'])&&
+isset($_POST['rmno'])&&
+isset($_POST['gr'])&&
+isset($_POST['gr_others'])&&
+isset($_POST['gr_to'])&&
+isset($_POST['gr_to_others'])&&
+isset($_POST['message']))
 {
 	$name = $_POST['name'];
 	$id = $_POST['id'];
@@ -29,13 +29,13 @@ if(isset($_POST['name']) &&
 
 
 	if(!empty($name) &&
-	   !empty($id) &&
-	   !empty($email) &&
-	   !empty($phno)&&
-	   !empty($rmno)&&
-	   !empty($gr)&&
-	   !empty($gr_to)&&
-	   !empty($message)) 	
+	!empty($id) &&
+	!empty($email) &&
+	!empty($phno)&&
+	!empty($rmno)&&
+	!empty($gr)&&
+	!empty($gr_to)&&
+	!empty($message))
 	{
 		if(($gr == 'Other' && empty($gr_others)) || ($gr_to == 'Other' && empty($gr_to_others)))
 		{
@@ -44,16 +44,16 @@ if(isset($_POST['name']) &&
 		else
 		{
 			$query = "INSERT INTO `cng`.`grievance_records` VALUES ('','".mysqli_real_escape_string($var,$name)."',
-												        '".mysqli_real_escape_string($var,$id)."',
-												        '".mysqli_real_escape_string($var,$email)."',
-												        '".mysqli_real_escape_string($var,$phno)."',
-												        '".mysqli_real_escape_string($var,$rmno)."',
-												        '".mysqli_real_escape_string($var,$gr)."',
-												        '".mysqli_real_escape_string($var,$gr_others)."',
-												        '".mysqli_real_escape_string($var,$gr_to)."',
-												        '".mysqli_real_escape_string($var,$gr_to_others)."',
-												        '".mysqli_real_escape_string($var,$message)."')";
-			
+			'".mysqli_real_escape_string($var,$id)."',
+			'".mysqli_real_escape_string($var,$email)."',
+			'".mysqli_real_escape_string($var,$phno)."',
+			'".mysqli_real_escape_string($var,$rmno)."',
+			'".mysqli_real_escape_string($var,$gr)."',
+			'".mysqli_real_escape_string($var,$gr_others)."',
+			'".mysqli_real_escape_string($var,$gr_to)."',
+			'".mysqli_real_escape_string($var,$gr_to_others)."',
+			'".mysqli_real_escape_string($var,$message)."')";
+
 			if($query_run = mysqli_query($var,$query))
 			{
 				$msg = "Your from has been submitted";
@@ -62,96 +62,75 @@ if(isset($_POST['name']) &&
 			{
 				die(mysqli_error($var));
 			}
-		}	
+		}
 	}
 	else{
 		$error2 = "Fill in all the fields";
 	}
 }
-	
+
 ?>
 
 <html>
-	<head>
-		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-      <!--Import materialize.css-->
-    	<link type="text/css" rel="stylesheet" href="../materialize/css/materialize.min.css"  media="screen,projection"/>
+<head>
+	<meta charset="utf-8">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script>
+	$(document).ready(function() {
+		$("#gr_others").hide();
 
-      <!--Let browser know website is optimized for mobile-->
-    	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-		<meta charset="utf-8">
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-		<script>
-			$(document).ready(function() {
+		$( "#gr_choice_3" ).change(function() {
+			var val = $("#gr_choice_3").val();
+			if(val=="Other"){
+				$("#gr_others").show();
+			} else {
 				$("#gr_others").hide();
+			}
+		});
+		$("#gr_to_others").hide();
 
-				$( "#gr_choice_3" ).change(function() {
-	  				var val = $("#gr_choice_3").val();
-					if(val=="Other"){
-					    $("#gr_others").show();
-					} else {
-					    $("#gr_others").hide();
-					}
-				});
+		$( "#gr_to_choice_3" ).change(function() {
+			var val = $("#gr_to_choice_3").val();
+			if(val=="Other"){
+				$("#gr_to_others").show();
+			} else {
 				$("#gr_to_others").hide();
+			}
+		});
+	});
+	</script>
+</head>
+<body>
+	<div class="row">
+		<form class="form" action="grievances.php" method="POST" enctype="multipart/form-data">
+			<h4>Greivance Form </h4>
+			<input type="text" name="name" value="<?php echo $name;?>" required/>
+			<input type="text" name="email" value="<?php echo $email;?>" required>
+			<input placeholder="ID Number" type="text" name="id" required>
+			<input placeholder="Phone Number" type="text" name="phno" required>
+			<input placeholder="Room Number" type="text" name="rmno" required>
 
-				$( "#gr_to_choice_3" ).change(function() {
-	  				var val = $("#gr_to_choice_3").val();
-					if(val=="Other"){
-					    $("#gr_to_others").show();
-					} else {
-					    $("#gr_to_others").hide();
-					}
-					});
-			});
-		</script>
-		<link rel="stylesheet" href="../css/grievances.css" />
-	</head>
-	<body>
-		<div class="row">	
-			<form class="form #e3f2fd blue lighten-5 z-depth-3" action="grievances.php" method="POST" enctype="multipart/form-data">
-				<h4>Greivance Form </h4>
-				<div class="div">
-					<div class="input-field col s12">
-						<input type="text" name="name" value="<?php echo $name;?>" required/>
-					</div>
-					<div class="input-field col s12">	
-						<input type="text" name="email" value="<?php echo $email;?>" required>
-					</div>
-					<div class="input-field col s12">	
-						<input placeholder="ID Number" type="text" name="id" required>
-					</div>
-					<div class="input-field col s12">	
-						<input placeholder="Phone Number" type="text" name="phno" required>
-					</div>	
-					<div class="input-field col s12">	
-						<input placeholder="Room Number" type="text" name="rmno" required>
-					</div>
-					<div class="input-field col s12" style="padding:0; font-size:17.5px;">Grievance On :  </div>
-							<p><div class="choice"><input type="radio" class="with-gap" id="gr_choice_1" name="gr" value="EC"><label for="gr_choice_1">EC</label><br></div></p>
-							<p><div class="choice"><input type="radio" class="with-gap" id="gr_choice_2" name="gr" value="CRC"><label for="gr_choice_2">CRC</label><br></div></p>
-							<p><div class="choice"><input type="radio" class="with-gap" id="gr_choice_4" name="gr" value="SU"><label for="gr_choice_4">SU</label><br></div></p>
-							<p><div class="choice"><input type="radio" class="with-gap" id="gr_choice_3" name="gr" value="Other"><label for="gr_choice_3">Others</label><br></div></p>
-							<input type="text" placeholder="Please Specify" name="gr_others" id="gr_others"><br>
-					
-					<div class="input-field col s12" style="padding-left:0; font-size:17.5px;">Submit Grievance to :</div>  
-							<p><div class="choice_to"><input type="radio" class="with-gap" id="gr_to_choice_1" name="gr_to" value="EC"><label for="gr_to_choice_1">EC</label><br></div></p>
-							<p><div class="choice_to"><input type="radio" class="with-gap" id="gr_to_choice_2" name="gr_to" value="CRC"><label for="gr_to_choice_2">CRC</label><br></div></p>
-							<p><div class="choice_to"><input type="radio" class="with-gap" id="gr_to_choice_4" name="gr_to" value="SU"><label for="gr_to_choice_4">SU</label><br></div></p>
-							<p><div class="choice_to"><input type="radio" class="with-gap" id="gr_to_choice_3" name="gr_to" value="Other"><label for="gr_to_choice_3">Others</label><br></div></p>
-							<input type="text" placeholder="Please Specify" name="gr_to_others" id="gr_to_others"><br>		
-					
-					
-					<div class="input-field col s12">		
-						<textarea placeholder="Grievance" class="materialize-textarea" name="message" placeholder="Type Your Message Here" rows="6" cols="40"></textarea>
-					</div>
-						<div><input class="z-depth-4 button" type="submit" name="submit" value="Submit"><a style="margin-left:35%; font-size:20px;" class="z-depth-4 button gr_anonymous">Fill Anonymously</a></div>	
-						<p style="margin:1%; text-align:center; color:red; font-size: 20px;"><?php echo $error1.$error2.$msg; ?></p>
-				</div>		
-			</form>
-		</div>
-		<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-  <script type="text/javascript" src="js/materialize.min.js"></script>	
-	</body>
+			<p>Grievance On :</p>
+			<p><div class="choice"><input type="radio" id="gr_choice_1" name="gr" value="EC"><label for="gr_choice_1">EC</label><br></div></p>
+			<p><div class="choice"><input type="radio" id="gr_choice_2" name="gr" value="CRC"><label for="gr_choice_2">CRC</label><br></div></p>
+			<p><div class="choice"><input type="radio" id="gr_choice_4" name="gr" value="SU"><label for="gr_choice_4">SU</label><br></div></p>
+			<p><div class="choice"><input type="radio" id="gr_choice_3" name="gr" value="Other"><label for="gr_choice_3">Others</label><br></div></p>
+			<input type="text" placeholder="Please Specify" name="gr_others" id="gr_others"><br>
+
+			<p>Submit Grievance to :</p>
+			<p><div class="choice_to"><input type="radio" id="gr_to_choice_1" name="gr_to" value="EC"><label for="gr_to_choice_1">EC</label><br></div></p>
+			<p><div class="choice_to"><input type="radio" id="gr_to_choice_2" name="gr_to" value="CRC"><label for="gr_to_choice_2">CRC</label><br></div></p>
+			<p><div class="choice_to"><input type="radio" id="gr_to_choice_4" name="gr_to" value="SU"><label for="gr_to_choice_4">SU</label><br></div></p>
+			<p><div class="choice_to"><input type="radio" id="gr_to_choice_3" name="gr_to" value="Other"><label for="gr_to_choice_3">Others</label><br></div></p>
+			<input type="text" placeholder="Please Specify" name="gr_to_others" id="gr_to_others"><br>
+
+			<textarea placeholder="Grievance" name="message" placeholder="Type Your Message Here" rows="6" cols="40"></textarea>
+
+			<input class="submit" type="submit" name="submit" value="Submit">
+			<input class="gr_anonymous submit" type="submit" value="Fill Anonymously">
+			<p><?php echo $error1.$error2.$msg; ?></p>
+		</form>
+	</div>
+	<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+</body>
 </html>
